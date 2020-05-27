aca7b6a3802e822c226d0a9c596f145f347889a3
furkan <furkan9708@gmail.com>
2020-05-27 18:28:23 +0300

    Incorrect folders fixed

--- lib/models/photo_model.dart
+++ lib/models/photo_model.dart
@@ -1,53 +1,18 @@
-import 'dart:convert';
+class Photo {
 
-import 'package:flutter/material.dart';
+  int albumId;
+  int id;
+  String title;
+  String url;
+  String thumbnailUrl;
 
-import 'package:http/http.dart' as http;
-import 'package:http_get_photos/photo.dart';
+  Photo.fromJsonMap(Map<String, dynamic> map): 
+    albumId = map["albumId"],
+    title=map["title"],
+    id = map["id"],
+    url = map["url"],
+    thumbnailUrl = map["thumbnailUrl"];
 
-class PhotoModel extends StatefulWidget {
-  @override
-  _PhotoModelState createState() => _PhotoModelState();
 }
 
-class _PhotoModelState extends State<PhotoModel>{
-  Future<List<Photo>> _getPhoto() async {
-  var response = await http.get("https://jsonplaceholder.typicode.com/photos");  
-  if (response.statusCode==200){
-    return (json.decode(response.body)as List)
-    .map((e) => Photo.fromJsonMap(e))
-    .toList();
-  }
-  else {
-    throw Exception("Bağlantı Başarısız Hata Kodu: ${response.statusCode}");
-  }
-  }
-  @override
-  Widget build(BuildContext context) {
-    return Scaffold(
-      appBar: AppBar( title: Text("Json Api "),),
-      body: FutureBuilder(
-        future: _getPhoto(),
-        builder: 
-        (BuildContext context, AsyncSnapshot<List<Photo>> snapshot) {
-          if(snapshot.hasData){
-            return ListView.builder(
-              itemCount: snapshot.data.length,
-              itemBuilder: (context, index){
-                return ListTile(
-                  title: Text(snapshot.data[index].title),
-                  leading: CircleAvatar(backgroundImage: NetworkImage(snapshot.data[index].url)),
-                  
-                  );
-                
-              });
-           }
 
-           else {
-             return Center(child: CircularProgressIndicator(),);
-           }
-        }),
-      
-    );
-  }
-}
